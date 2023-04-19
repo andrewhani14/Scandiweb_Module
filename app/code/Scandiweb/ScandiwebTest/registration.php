3a9daf7f083012a845fa1c9695eb219f1a81c228
Andrew Hani <56038059+andrewhani14@users.noreply.github.com>
2023-04-19 16:48:57 +0200

    Update registration.php

--- app/code/Scandiweb/ScandiwebTest/registration.php
+++ app/code/Scandiweb/ScandiwebTest/registration.php
@@ -1,5 +1,6 @@
 <?php \Magento\Framework\Component\ComponentRegistrar::register(
 	\Magento\Framework\Component\ComponentRegistrar::MODULE,
+    \Magento\Framework\Component\ComponentRegistrar::Class
 	'Scandiweb_ScandiwebTest',
 	__DIR__
 );
